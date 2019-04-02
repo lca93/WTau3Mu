7bf8b5cf0c45e2e6c8fe633da92591b33a97b938
Luca Guzzi <luca.guzzi@cern.ch>
2019-04-02 15:42:14 +0200

    bugfix
    	modified:   python/samples/mc_2017.py

--- python/samples/mc_2017.py
+++ python/samples/mc_2017.py
@@ -7,16 +7,19 @@ import glob
 from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
 kreator = ComponentCreator()
 
-//Pythia MC
+## Pythia MC
+files_pythia = glob.glob('/eos/user/l/lguzzi/WTau3Mu/samples/2017/MC/*.root')
+
 WToTauTo3Mu = kreator.makePrivateMCComponent(
     name       = 'WToTauTo3Mu',
     dataset    = '/WToTauNu_Tau3Mu_Pythia/lguzzi-CRAB3_MC_generation_40k_miniaod_smallfiles-5f646ecd4e1c7a39ab0ed099ff55ceb9/USER',
+    files      = files_pythia,
     prefix = 'root://cms-xrd-global.cern.ch',
     xSec       = 20508.9 * 1.e-7, # W to lep nu / 3.[pb] x BR
 )
-WToTauTo3Mu.files = glob.glob('/eos/user/l/lguzzi/WTau3Mu/samples/2017/MC/*.root')
+WToTauTo3Mu.files = files_pythia
 
-//MadGraph MC
+## MadGraph MC
 WToTauTo3Mu_MG = kreator.makeMCComponent(
     name       = 'WToTauTo3Mu',
     dataset    = '/WToTauNu_TauTo3Mu_MadGraph_13TeV/PhaseIFall16MiniAOD-FlatPU28to62HcalNZSRAW_PhaseIFall16_90X_upgrade2017_realistic_v6_C1-v1/MINIAODSIM',
