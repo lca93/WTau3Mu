bc436b8973acd2618bb77aad3a11454f53c933ef
Luca <luca.guzzi@mib.infn.it>
2020-05-06 16:20:34 +0200

    updated mc files 	modified:   python/samples/mc_2017.py 	modified:   python/samples/mc_2018.py

--- python/samples/mc_2017.py
+++ python/samples/mc_2017.py
@@ -8,24 +8,20 @@ from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
 kreator = ComponentCreator()
 
 ## Pythia MC
-bad_files = [
-    '/gwteras/cms/store/user/lguzzi/WToTauNu_Tau3Mu_Pythia/CRAB3_MC_generation_300k_miniaod_smallfiles/190313_143423/0003/WTau3Mu-RunIIFall17_MINIAODSIM_3314.root',
-]
+files_2017_300k = glob.glob('/gwteras/cms/store/user/lguzzi/WToTauNu_Tau3Mu_Pythia_RunIIFall17/crab_miniaod/200502_104949/0000/wtotaunu_tau3mu_phytia_RunIIAutumn17MiniAOD_*.root')
 
-files_2017_300k = glob.glob('/gwteras/cms/store/user/lguzzi/WToTauNu_Tau3Mu_Pythia/CRAB3_MC_generation_300k_miniaod_smallfiles/190313_143423/*/*.root')
-files_2017_300k = [ff for ff in files_2017_300k if not ff in bad_files]
+# first private production (BR bug)
+#files_2017_300k = glob.glob('/gwteras/cms/store/user/lguzzi/WToTauNu_Tau3Mu_Pythia/CRAB3_MC_generation_300k_miniaod_smallfiles/190313_143423/*/*.root')
 
-# first private production
+## correct private production
 WToTauTo3Mu_Pythia = cfg.MCComponent(
     dataset       = 'WToTauTo3Mu',
     name          = 'WToTauTo3Mu',
     files         = files_2017_300k ,
     xSection      = 21490.9, # this uses the correct tau BR from the PDG # 20508.9 * 1.e-7, # W to lep nu / 3.[pb] x BR
-#     xSection      = 20508.9 * 0.005, # W to lep nu / 3.[pb] x BR
-    nGenEvents    = 342200,
+    nGenEvents    = 390032,
     effCorrFactor = 1,
 )
-
 ## central production 
 WToTauTo3Mu_MadGraph = kreator.makeMCComponent(
     name        = 'WToTauTo3Mu' ,
--- python/samples/mc_2018.py
+++ python/samples/mc_2018.py
@@ -8,22 +8,19 @@ from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
 kreator = ComponentCreator()
 
 ## Pythia MC
-bad_files = [
-]
+files_2018_400k = glob.glob('/gwteras/cms/store/user/lguzzi/WToTauNu_Tau3Mu_Pythia_RunIIFall18/crab_digireco_step2/200501_160511/0000/wtotaunu_tau3mu_phytia_RunIIAutumn18MiniAOD_*.root')
+# first private production (BR bug)
+#files_2018_400k = glob.glob('/gwteras/cms/store/user/lguzzi/WToTauNu_Tau3Mu_Pythia/CRAB3_MC_generation_400k_miniaod_smallfiles/190321_150932/*/*.root')
 
-files_2018_400k = glob.glob('/gwteras/cms/store/user/lguzzi/WToTauNu_Tau3Mu_Pythia/CRAB3_MC_generation_400k_miniaod_smallfiles/190321_150932/*/*.root')
-files_2018_400k = [ff for ff in files_2018_400k if not ff in bad_files]
-
-# first private production
 WToTauTo3Mu_Pythia = cfg.MCComponent(
     dataset       = 'WToTauTo3Mu',
     name          = 'WToTauTo3Mu',
     files         = files_2018_400k ,
     xSection      = 21490.9, # this uses the correct tau BR from the PDG # 20508.9 * 1.e-7, # W to lep nu / 3.[pb] x BR
-#     xSection      = 20508.9 * 0.005, # W to lep nu / 3.[pb] x BR
-    nGenEvents    = 342200,
+    nGenEvents    = 381627,
     effCorrFactor = 1,
 )
+
 
 # use this one, larger stats!
 WToTauTo3Mu_MadGraph = kreator.makeMCComponent(
@@ -32,14 +29,4 @@ WToTauTo3Mu_MadGraph = kreator.makeMCComponent(
     user        = 'CMS'         , 
     pattern     = '.*root'      ,
     useAAA      = True          ,
-)
-
-WToTauTo3Mu_Pythia_taumass1p65 = cfg.MCComponent(
-    dataset       = 'WToTauTo3Mu',
-    name          = 'WToTauTo3Mu',
-    files         = '/gwpool/users/lguzzi/Tau3Mu/2017_2018/MC_prod/mc_2017/different_mass/miniaod/WTau3Mu-RunIIFall17_MINIAODSIM.root' ,
-    xSection      = 21490.9, # this uses the correct tau BR from the PDG # 20508.9 * 1.e-7, # W to lep nu / 3.[pb] x BR
-#     xSection      = 20508.9 * 0.005, # W to lep nu / 3.[pb] x BR
-    nGenEvents    = 100,
-    effCorrFactor = 1,
 )
