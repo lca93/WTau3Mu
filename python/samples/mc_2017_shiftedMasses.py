262d9799a0faf83b7d455b156223ed1f37c12c32
Luca <luca.guzzi@mib.infn.it>
2020-10-02 12:11:30 +0200

    adding UL shifted

---
+++ python/samples/mc_2017_shiftedMasses.py
@@ -0,0 +1,105 @@
+import PhysicsTools.HeppyCore.framework.config as cfg
+import os
+import glob
+
+#####COMPONENT CREATOR
+
+from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
+kreator = ComponentCreator()
+
+WToTauTo3Mu_Pythia_taumass1p65 = cfg.MCComponent(
+    dataset       = 'WToTauTo3Mu_1p65',
+    name          = 'WToTauTo3Mu_1p65',
+    files         = '/gwpool/users/lguzzi/Tau3Mu/2017_2018/MC_prod/MINIAODSIM/2017/WTau3Mu-RunIIFall17GS_MINIAODSIM_tauMass1p65.root',
+    xSection      = 21490.9,
+    nGenEvents    = 50000,
+    effCorrFactor = 1,
+)
+WToTauTo3Mu_Pythia_taumass1p70 = cfg.MCComponent(
+    dataset       = 'WToTauTo3Mu_1p70',
+    name          = 'WToTauTo3Mu_1p70',
+    files         = '/gwpool/users/lguzzi/Tau3Mu/2017_2018/MC_prod/MINIAODSIM/2017/WTau3Mu-RunIIFall17GS_MINIAODSIM_tauMass1p70.root',
+    xSection      = 21490.9,
+    nGenEvents    = 50000,
+    effCorrFactor = 1,
+)
+WToTauTo3Mu_Pythia_taumass1p85 = cfg.MCComponent(
+    dataset       = 'WToTauTo3Mu_1p85',
+    name          = 'WToTauTo3Mu_1p85',
+    files         = '/gwpool/users/lguzzi/Tau3Mu/2017_2018/MC_prod/MINIAODSIM/2017/WTau3Mu-RunIIFall17GS_MINIAODSIM_tauMass1p85.root',
+    xSection      = 21490.9,
+    nGenEvents    = 50000,
+    effCorrFactor = 1,
+)
+WToTauTo3Mu_Pythia_taumass1p90 = cfg.MCComponent(
+    dataset       = 'WToTauTo3Mu_1p90',
+    name          = 'WToTauTo3Mu_1p90',
+    files         = '/gwpool/users/lguzzi/Tau3Mu/2017_2018/MC_prod/MINIAODSIM/2017/WTau3Mu-RunIIFall17GS_MINIAODSIM_tauMass1p90.root',
+    xSection      = 21490.9,
+    nGenEvents    = 50000,
+    effCorrFactor = 1,
+)
+WToTauTo3Mu_Pythia_taumass1p95 = cfg.MCComponent(
+    dataset       = 'WToTauTo3Mu_1p95',
+    name          = 'WToTauTo3Mu_1p95',
+    files         = '/gwpool/users/lguzzi/Tau3Mu/2017_2018/MC_prod/MINIAODSIM/2017/WTau3Mu-RunIIFall17GS_MINIAODSIM_tauMass1p95.root',
+    xSection      = 21490.9,
+    nGenEvents    = 50000,
+    effCorrFactor = 1,
+)
+
+WToTauTo3Mu_Pythia_shiftedMasses = [
+    WToTauTo3Mu_Pythia_taumass1p65,
+    WToTauTo3Mu_Pythia_taumass1p70,
+    WToTauTo3Mu_Pythia_taumass1p85,
+    WToTauTo3Mu_Pythia_taumass1p90,
+    WToTauTo3Mu_Pythia_taumass1p95,
+]
+
+WToTauTo3Mu_PythiaUL_taumass1p95 = kreator.makeMCComponent(
+    name    = 'WToTauTo3Mu_1p95',
+    dataset = '/W_ToTau_ToMuMuMu_MTau1950_TuneCP5_13TeV-pythia8/RunIISummer19UL17MiniAOD-106X_mc2017_realistic_v6-v2/MINIAODSIM',
+    user    = 'CMS',
+    pattern = '.*root',
+    useAAA  = True,
+)
+
+WToTauTo3Mu_PythiaUL_taumass1p90 = kreator.makeMCComponent(
+    name    = 'WToTauTo3Mu_1p90',
+    dataset = '/W_ToTau_ToMuMuMu_MTau1900_TuneCP5_13TeV-pythia8/RunIISummer19UL17MiniAOD-106X_mc2017_realistic_v6-v2/MINIAODSIM',
+    user    = 'CMS',
+    pattern = '.*root',
+    useAAA  = True,
+)
+
+WToTauTo3Mu_PythiaUL_taumass1p85 = kreator.makeMCComponent(
+    name    = 'WToTauTo3Mu_1p85',
+    dataset = '/W_ToTau_ToMuMuMu_MTau1850_TuneCP5_13TeV-pythia8/RunIISummer19UL17MiniAOD-106X_mc2017_realistic_v6-v2/MINIAODSIM',
+    user    = 'CMS',
+    pattern = '.*root',
+    useAAA  = True,
+)
+
+WToTauTo3Mu_PythiaUL_taumass1p70 = kreator.makeMCComponent(
+    name    = 'WToTauTo3Mu_1p70',
+    dataset = '/W_ToTau_ToMuMuMu_MTau1700_TuneCP5_13TeV-pythia8/RunIISummer19UL17MiniAOD-106X_mc2017_realistic_v6-v2/MINIAODSIM',
+    user    = 'CMS',
+    pattern = '.*root',
+    useAAA  = True,
+)
+
+WToTauTo3Mu_PythiaUL_taumass1p65 = kreator.makeMCComponent(
+    name    = 'WToTauTo3Mu_1p65',
+    dataset = '/W_ToTau_ToMuMuMu_MTau1650_TuneCP5_13TeV-pythia8/RunIISummer19UL17MiniAOD-106X_mc2017_realistic_v6-v2/MINIAODSIM',
+    user    = 'CMS',
+    pattern = '.*root',
+    useAAA  = True,
+)
+
+WToTauTo3Mu_Pythia_UltraLegacy_shiftedMasses = [ 
+    WToTauTo3Mu_PythiaUL_taumass1p95,
+    WToTauTo3Mu_PythiaUL_taumass1p90,
+    WToTauTo3Mu_PythiaUL_taumass1p85,
+    WToTauTo3Mu_PythiaUL_taumass1p70,
+    WToTauTo3Mu_PythiaUL_taumass1p65,
+]
