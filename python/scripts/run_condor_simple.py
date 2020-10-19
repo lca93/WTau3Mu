311960dbe01000d9dd1c5c5f76e4597bc886c40b
Luca <luca.guzzi@mib.infn.it>
2020-10-19 11:49:16 +0200

    small update

--- python/scripts/run_condor_simple.py
+++ python/scripts/run_condor_simple.py
@@ -3,6 +3,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--batchname'   , default = ''      , type = str, help = 'name to display')
 parser.add_argument('--script'      , required = True   , type = str, help = 'name of the script to launch')
+parser.add_argument('--veto'        , default = ''      , type = str, help = 'list of vetoed machines (separated by a comma)')
 time_group = parser.add_mutually_exclusive_group()
 time_group.add_argument('--runtime' , default = 3000    , type = int, help = 'runtime in minutes')
 time_group.add_argument('--flavour' , default = ''      , type = str, help = 'job flavour')
@@ -14,15 +15,18 @@ import htcondor
 schedd = htcondor.Schedd()
 submit = htcondor.Submit()
 
-submit['Universe']            = 'Vanilla'
-submit['Executable']          = args.script
-submit['use_x509userproxy']   = 'true'
-submit['Log']                 = 'condor_job.log'
-submit['Output']              = 'condor_job.out'
-submit['Error']               = 'condor_job.err'
-submit['getenv']              = 'true'
-submit['environment']         = "LS_SUBCWD={}".format(os.environ['PWD'])
-submit['request_memory']      = 2000
+submit['Universe']          = 'Vanilla'
+submit['Executable']        = args.script
+submit['use_x509userproxy'] = 'true'
+submit['Log']               = 'condor_job.log'
+submit['Output']            = 'condor_job.out'
+submit['Error']             = 'condor_job.err'
+submit['getenv']            = 'true'
+submit['environment']       = "LS_SUBCWD={}".format(os.environ['PWD'])
+submit['request_memory']    = 2000
+
+if not args.veto is '':
+    submit['+Requirements'] = ' && '.join(['(machine != "{}")'.format(mac.strip(' ')) for mac in args.veto.split(',')])
 
 if not args.flavour is '': 
     submit['+JobFlavour'] = args.flavour
@@ -34,5 +38,3 @@ if not args.batchname is '':
 
 with schedd.transaction() as txn:
   submit.queue(txn)
-
-print '[INFO] Jobs submitted inside directory', os.environ['PWD']
