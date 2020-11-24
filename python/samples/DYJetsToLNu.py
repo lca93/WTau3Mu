35c211cca59e16dfe480d7a0bdd3e02def36bc4d
Luca <luca.guzzi@mib.infn.it>
2020-11-24 17:04:58 +0100

    dyjetstolnu

---
+++ python/samples/DYJetsToLNu.py
@@ -0,0 +1,16 @@
+import PhysicsTools.HeppyCore.framework.config as cfg
+import os
+import glob
+
+#####COMPONENT CREATOR
+
+from CMGTools.RootTools.samples.ComponentCreator import ComponentCreator
+kreator = ComponentCreator()
+
+DYJetsToLL_M50_2018 = kreator.makeMCComponent(
+    name        = 'DYJetsToLNu_M50' ,
+    dataset     = '/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM',
+    user        = 'CMS'         , 
+    pattern     = '.*root'      ,
+    useAAA      = True          ,
+) ; DYJetsToLL_M50.nGenEvents = 100194597
